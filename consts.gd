91881cfa5998ce686c65edc2cda37c12f3dc7377
DragonXVI <Dragonboyxvi@gmail.com>
2026-06-10 23:39:04 -0700

    delete unneeded csharp files

--- consts.gd
+++ consts.gd
@@ -4,13 +4,16 @@ extends Object;
 class_name Consts;
 
 
+static func _static_init() -> void:
+	_editor_layer_names();
+
+
 ## Smallest size of the game display viewport.
 ## Also the size of the play area ingame.
 const SCREEN_SIZE: Vector2i = Vector2i(
 	480,
 	720
 );
-
 
 ## Visual layers for objects to draw on.
 enum ZLayers {
@@ -22,3 +25,16 @@ enum ZLayers {
 	
 	BULLETS = 6,
 }
+
+## Godot collision layers
+enum Collision {
+	
+	PLAYER_HITBOX = 1<<0,
+	ENEMY_HITBOX = 1<<1,
+	
+}
+static func _editor_layer_names() -> void:
+	
+	const PATH := "layer_names/2d_physics/layer_%s";
+	ProjectSettings.set_setting( PATH % 1, "Player Hitbox" );
+	ProjectSettings.set_setting( PATH % 2, "Enemy Hitbox" );
