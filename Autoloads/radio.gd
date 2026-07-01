67c25b9ee23c2170e03bc7872bceb4c0c754aca9
DragonXVI <Dragonboyxvi@gmail.com>
2026-07-01 14:35:32 -0700

    add cashew shader

--- Autoloads/radio.gd
+++ Autoloads/radio.gd
@@ -27,9 +27,9 @@ func emit_settings_changed() -> void:
 
 #region Bullets
 
-signal bullet_fired( bullet: Bullet );
-func fire_bullet( bullet: Bullet ) -> void:
-	bullet_fired.emit( bullet );
+#signal bullet_fired( bullet: Bullet );
+#func fire_bullet( bullet: Bullet ) -> void:
+#	bullet_fired.emit( bullet );
 
 #signal bullet_hit_someone( bullet: Bullet, Owner: Node, Target: Node );
 
