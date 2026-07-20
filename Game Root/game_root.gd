b07b19ea200cf3c52c2dae0e0bc6914eed75677d
DragonXVI <Dragonboyxvi@gmail.com>
2026-07-20 17:22:52 -0400

    game root can change current room

--- Game Root/game_root.gd
+++ Game Root/game_root.gd
@@ -2,3 +2,25 @@ extends Node2D
 
 
 var _current_game_room: Node2D;
+
+
+func _ready() -> void:
+	
+	_change_room( "res://test_world.tscn" );
+
+
+func _change_room( room_path: String ) -> void:
+	assert( ResourceLoader.exists( room_path, "PackedScene" ) );
+	
+	#TODO fade screen here
+	
+	if ( _current_game_room ):
+		_current_game_room.queue_free();
+	
+	var packed_scene: PackedScene = load( room_path );
+	assert( packed_scene.can_instantiate() );
+	
+	_current_game_room = packed_scene.instantiate();
+	add_child( _current_game_room );
+	
+	#TODO unfade screen here
