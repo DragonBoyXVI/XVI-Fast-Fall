19ce8870e3d1fd9b98bf7bf0b4962ad9b1cd8375
DragonXVI <Dragonboyxvi@gmail.com>
2026-07-19 20:54:12 -0400

    player has i frames

--- Test Player/test_player.gd
+++ Test Player/test_player.gd
@@ -11,11 +11,19 @@ const BULLET_SCENE: PackedScene = preload( "uid://b05smqo7otih0" );
 
 
 @export var _speed: float = 400.0;
+
+@export_group( "Dashing", "_dash" )
 @export var _dash_duration: float = 0.125;
 @export var _dash_mult: float = 3.0;
 @export var _dash_cooldown: float = 2.0;
+
+@export_group( "" )
+@export var _damage_immune_time: float  = 1.0:
+	set( new ):
+		_damage_immune_time = maxf( 0.05, new );
 @export var _time_between_shots: float = 0.125;
 
+@export_group( "Components" )
 @export var _health_node: HealthNode
 
 
@@ -28,6 +36,9 @@ var _can_shoot: bool = true;
 var _dash_duration_timer: Timer;
 var _dash_cooldown_timer: Timer;
 var _shoot_cooldown_timer: Timer;
+
+var _damage_immune_timer: Timer;
+var _is_damage_immune: bool = false;
 
 
 func _ready() -> void:
@@ -45,6 +56,17 @@ func _ready() -> void:
 	_shoot_cooldown_timer = CooldownTimer.new();
 	add_child( _shoot_cooldown_timer, false, Node.INTERNAL_MODE_BACK );
 	_shoot_cooldown_timer.timeout.connect( _on_shoot_cooldown_timer_timeout, CONNECT_DEFERRED );
+	
+	_damage_immune_timer = CooldownTimer.new();
+	add_child( _damage_immune_timer, false, Node.INTERNAL_MODE_BACK );
+	_damage_immune_timer.timeout.connect( _on_damage_immune_timer_timeout, CONNECT_DEFERRED );
+
+func _process( _delta: float ) -> void:
+	
+	if ( _is_damagable() ):
+		modulate = Color.WHITE;
+	else:
+		modulate = Color.REBECCA_PURPLE;
 
 func _physics_process( delta: float ) -> void:
 	
@@ -72,8 +94,6 @@ func _do_dash() -> void:
 	_state = PState.DASH;
 	_dash_duration_timer.start( _dash_duration );
 	_dash_cooldown_timer.start( _dash_cooldown );
-	
-	modulate = Color.REBECCA_PURPLE;
 
 func _shoot() -> void:
 	if ( not _can_shoot ): return
@@ -87,9 +107,15 @@ func _shoot() -> void:
 	bullet.reset_physics_interpolation();
 
 
+func _is_damagable() -> bool:
+	if ( _state == PState.DASH ): return false;
+	if ( _is_damage_immune ): return false;
+	
+	return true;
+
+
 func _on_dash_duration_timer_timeout() -> void:
 	_state = PState.FREE;
-	modulate = Color.WHITE;
 
 func _on_dash_cooldown_timer_timeout() -> void:
 	_can_dash = true;
@@ -99,9 +125,15 @@ func _on_shoot_cooldown_timer_timeout() -> void:
 
 
 func _on_hitbox_took_damage( damage: int ) -> void:
-	if ( _state == PState.DASH ): return;
+	if ( not _is_damagable() ): return;
 	
 	_health_node.damage( damage );
+	_is_damage_immune = true;
+	_damage_immune_timer.start( _damage_immune_time );
 
 func _on_health_node_died() -> void:
 	queue_free();
+
+
+func _on_damage_immune_timer_timeout() -> void:
+	_is_damage_immune = false;
