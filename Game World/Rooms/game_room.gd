a0b2932351e61d3fc6819c5fcea72046d29d37bd
DragonXVI <MistyWyvern@pm.me>
2026-08-18 09:39:54 -0400

    pre pull commit

---
+++ Game World/Rooms/game_room.gd
@@ -0,0 +1,9 @@
+@tool
+extends Room;
+class_name GameRoom;
+## A room where a round of play occurs.
+##
+## Spawns a player and a quota
+
+
+const PLAYER_SCENE: PackedScene = preload( "uid://pqntxoy6xkil" );
