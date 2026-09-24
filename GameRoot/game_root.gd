320e597b5d3144685da4b7e488e5f04ee78ddd79
DragonXVI <DragonBoyXVI@gmail.com>
2026-09-24 18:14:37 -0400

    realizing i didnt need to do that yet uuuhhh whoops

--- GameRoot/game_root.gd
+++ GameRoot/game_root.gd
@@ -1,7 +1,21 @@
 extends Node2D;
 
 
+const DEFAULT_SCREEN_COVER_PATH := "uid://cupsm6cx3cwq2";
+
+
+@export var _screen_covers_root: Control;
 ## used to hide any visual burps from changing the cover scene.
 @export var _true_screen_cover: Control;
 ## used to play screen un/cover animations
 var _screen_cover: ObjScreenCover;
+
+
+func _ready() -> void:
+	
+	if ( Engine.is_editor_hint() ):
+		return;
+	
+	var loaded_cover: PackedScene = load( DEFAULT_SCREEN_COVER_PATH );
+	_screen_cover = loaded_cover.instantiate();
+	_screen_covers_root.add_child( _screen_cover );
