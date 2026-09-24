7def64b915d84b5f894ff8e740507cb7f5dca783
DragonXVI <DragonBoyXVI@gmail.com>
2026-09-24 17:35:27 -0400

    add todo

--- Player/States/player_state_free.gd
+++ Player/States/player_state_free.gd
@@ -6,7 +6,7 @@ class_name PlayerStateFree;
 @export var _movement: MovementComponent;
 
 
-func _physics_process( delta: float ) -> void:
+func _physics_process( _delta: float ) -> void:
 	
 	var input_dir: Vector2 = InputNames.Move.get_dir();
 	_movement.velocity_dir = input_dir;
--- Player/player.gd
+++ Player/player.gd
@@ -5,5 +5,5 @@ class_name ObjPlayer;
 const STATE_FREE := &"PlayerStateFree";
 
 
-func _physics_process( delta: float ) -> void:
+func _physics_process( _delta: float ) -> void:
 	position = position.clamp( Vector2.ZERO, Consts.SCREEN_SIZE );
